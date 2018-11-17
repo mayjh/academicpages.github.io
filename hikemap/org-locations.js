06a9762b74373e0a1250d2101fd17472a8a1074a
mayjh <jianhongshen33@gmail.com>
2018-11-17 15:15:26 -0600

    add more locations

--- hikemap/org-locations.js
+++ hikemap/org-locations.js
@@ -10,12 +10,23 @@ var addressPoints = [
     -88.222117
   ],
   [
+    "Radnor Lake State Park, Nashville, TN, USA",
+    36.063187,
+    -86.809667
+  ],
+  [
+    "Percey Warner Park, Nashville, TN, USA",
+    36.061128,
+    -86.897977
+  ],
+
+
+
+  [
     "Sulphur Springs, Malgretoute, St. Lucia",
     13.839226,
     -61.047077
   ],
-
-
   [
     "Rocky Mountain National Park, Grand Lake, CO, USA",
     40.246794,
@@ -41,10 +52,83 @@ var addressPoints = [
     36.102853,
     -115.170253
   ],
+  [
+    "Central Park, New York, NY, USA",
+    40.782770,
+    -73.965269
+  ],
+  [
+    "Rutgers University, Newark, NJ, USA",
+    40.741238,
+    -74.173415
+  ],
+  [
+    "Everglades National Park, Homestead, FL, USA",
+    25.293309,
+    -80.897428
+  ],
+  [
+    "Southernmost Point of the Continental US, Key West, FL, USA",
+    24.563736,
+    -81.797124
+  ],
+  [
+    "Universal Studio Florida, Orlando, FL, USA",
+    28.473430,
+    -81.464906
+  ],
+  [
+    "Congaree National Park, Hopkins, SC, USA",
+    33.792363,
+    -80.768787
+  ],
+  [
+    "Middleton Place Plantation, Charlston, SC, USA",
+    32.807235,
+    -79.955702
+  ],
+  [
+    "National Civil Rights Museum, Memphis, TN, USA",
+    35.134719,
+    -90.057507
+  ],
+  [
+    "Monument Valley, Oljato-Monument Valley, AZ, USA",
+    36.998337,
+    -110.098425
+  ],
+  [
+    "Antelope Canyon, Page, AZ, USA",
+    36.863973,
+    -111.374202
+  ],
+  [
+    "Bryce Canyon National Park, Bryce, UT, USA",
+    37.598810,
+    -112.192693
+  ],
+  [
+    "Zion National Park, Springdale, UT, USA",
+    37.300064,
+    -113.027485
+  ],
+
 
 
   [
-    "峨眉山, Mount Emei, Emeishan, Leshan, Sichuan, China",
+    "Stanley Park, Vancouver, BC, Canada",
+    49.305425,
+    -123.141438
+  ],
+  [
+    "Capilano Suspension Bridge Park, North Vancouver, BC, Canada",
+    49.346317,
+    -123.114311
+  ],
+
+
+  [
+    "峨眉山, Emei Mountain, Emeishan, Leshan, Sichuan, China",
     29.520450,
     103.339469
   ],
@@ -52,5 +136,10 @@ var addressPoints = [
     "潭柘寺, Tanzhe Tower, Beijing, China",
     39.901041,
     116.030653
+  ],
+  [
+    "泰山, Tai Mountain, Tai'an, Shandong, China",
+    36.274077,
+    117.105424
   ]
 ];
