60b107d4818f1db9978401aac4e3a9ab704e7717
mayjh <jianhongshen33@gmail.com>
2018-11-17 14:24:04 -0600

    add some locations

--- hikemap/org-locations.js
+++ hikemap/org-locations.js
@@ -1,11 +1,50 @@
 var addressPoints = [
   [
-    "[Fiery Gizzard Trail](https://mayjh.github.io/), Tracy City, TN, USA",
+    "Fiery Gizzard Trail, Tracy City, TN, USA",
     35.262041,
     -85.746334
   ],
   [
-    "Mount Emei, Emeishan, Leshan, Sichuan, China",
+    "Land Between the Lakes, Grand Rivers, KY, USA",
+    36.986705,
+    -88.222117
+  ],
+  [
+    "Sulphur Springs, Malgretoute, St. Lucia",
+    13.839226,
+    -61.047077
+  ],
+
+
+  [
+    "Rocky Mountain National Park, Grand Lake, CO, USA",
+    40.246794,
+    -105.839204
+  ],
+  [
+    "Box Springs Mountain Reserve Park, Riverside, CA, USA",
+    33.981308,
+    -117.291468
+  ],
+  [
+    "Grand Canyon National Park, Grand Canyon, AZ, USA",
+    36.116831,
+    -112.049855
+  ],
+  [
+    "Death Valley National Park, Death Valley, CA, USA",
+    36.517253,
+    -117.109626
+  ],
+  [
+    "Las Vegas Strip, Las Vegas, NV, USA",
+    36.102853,
+    -115.170253
+  ],
+
+
+  [
+    "峨眉山, Mount Emei, Emeishan, Leshan, Sichuan, China",
     29.520450,
     103.339469
   ],
@@ -13,10 +52,5 @@ var addressPoints = [
     "潭柘寺, Tanzhe Tower, Beijing, China",
     39.901041,
     116.030653
-  ],
-  [
-    "Box Springs Mountain Reserve Park, Riverside, CA, USA",
-    33.981308,
-    -117.291468
   ]
 ];
