78a310f2964c009a49a38843e2c65ed6c51a71df
mayjh <jianhongshen33@gmail.com>
2018-11-17 13:56:58 -0600

    add a few other locations

--- hikemap/org-locations.js
+++ hikemap/org-locations.js
@@ -5,18 +5,18 @@ var addressPoints = [
     -85.746334
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Mount Emei, Emeishan, Leshan, Sichuan, China",
+    29.520450,
+    103.339469
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "潭柘寺, Tanzhe Tower, Beijing, China",
+    39.901041,
+    116.030653
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Box Springs Mountain Reserve Park, Riverside, CA, USA",
+    33.981308,
+    -117.291468
   ]
 ];
